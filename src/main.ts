ec894d8e2c3baa3719ac832305c0ea607913304d
Nour-Gamal <nmg181963@hotmail.com>
2020-09-19 20:01:58 +0200

    V1.0

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,23 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import {store} from "./store";
+import { store } from "./store";
 import vuetify from "./plugins/vuetify";
-import * as firebase from "firebase"
+import * as firebase from "firebase";
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App),
-  created(){
+  render: (h) => h(App),
+  created() {
     firebase.initializeApp({
-      apiKey: 'AIzaSyA5t9DOlv2qiGOqmJG3NKuBtr8N9rdUhr0',
-      authDomain: 'nutopia-meetup.firebaseapp.com',
-      databaseURL: 'https://nutopia-meetup.firebaseio.com',
-      projectId: 'nutopia-meetup',
-      storageBucket: '',
-    })
-  }
+      apiKey: "AIzaSyA5t9DOlv2qiGOqmJG3NKuBtr8N9rdUhr0",
+      authDomain: "nutopia-meetup.firebaseapp.com",
+      databaseURL: "https://nutopia-meetup.firebaseio.com",
+      projectId: "nutopia-meetup",
+      storageBucket: "",
+    });
+  },
 }).$mount("#app");
